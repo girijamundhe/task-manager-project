b227fbef8655645392f61de31b43e844f07b8fb2
girija mundhe <girijamundhe1@gmail.com>
2026-05-07 16:34:05 +0530

    Completed Week 2 authentication APIs

---
+++ login.js
@@ -0,0 +1,11 @@
+function loginUser() {
+  const email = "girija@gmail.com";
+  const password = "123456";
+
+  if (email && password) {
+    console.log("Login Successful");
+    console.log("Token Generated");
+  }
+}
+
+loginUser();
