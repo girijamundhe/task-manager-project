735c4aacedb8af3e2de5f7f64c0356d9762472c6
girija mundhe <girijamundhe1@gmail.com>
2026-05-07 16:23:52 +0530

    completed week 1 task

---
+++ task.js
@@ -0,0 +1,9 @@
+const Task = {
+  title: "Complete Backend Setup",
+  description: "Create models for internship",
+  status: "Pending",
+  due_date: "2026-05-10",
+  assigned_user: "Girija"
+};
+
+console.log(Task);
