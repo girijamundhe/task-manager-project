c5c7351ac505136a68d096add57fe0cc7a41d793
girija mundhe <girijamundhe1@gmail.com>
2026-05-07 16:32:39 +0530

    Completed Week 2 authentication APIs

---
+++ register.js
@@ -0,0 +1,11 @@
+function registerUser() {
+  const user = {
+    name: "Girija",
+    email: "girija@gmail.com",
+    password: "123456"
+  };
+
+  console.log("User Registered:", user);
+}
+
+registerUser();
